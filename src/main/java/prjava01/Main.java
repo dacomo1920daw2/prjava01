44cb1d885df8c1df1e5d886a94f8e0398c761377
dacomo <dacomo@daw2m08.fjeclot.net>
2019-09-26 03:51:09 +0200

    "Versió 0.1 del projecte prjava01

---
+++ src/main/java/prjava01/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prjava01;
+
+/**
+ *
+ * @author dacomo1920daw2
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola a tothom");
+    }   
+}
